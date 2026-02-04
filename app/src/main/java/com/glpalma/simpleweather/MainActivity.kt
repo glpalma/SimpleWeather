543438abc83b5d662ca8e5d810c478a1b25f653b
Gustavo Palma <gustavolpalma@gmail.com>
2026-02-04 11:20:35 -0300

    Updated WeatherScreen to make it previewable

--- app/src/main/java/com/glpalma/simpleweather/MainActivity.kt
+++ app/src/main/java/com/glpalma/simpleweather/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.glpalma.simpleweather.ui.weather.WeatherScreen
+import com.glpalma.simpleweather.ui.weather.WeatherRoute
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WeatherScreen()
+            WeatherRoute()
         }
     }
 }
---
+++ app/src/main/java/com/glpalma/simpleweather/ui/weather/WeatherRoute.kt
@@ -0,0 +1,104 @@
+package com.glpalma.simpleweather.ui.weather
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.glpalma.simpleweather.domain.model.CurrentWeather
+import com.glpalma.simpleweather.domain.model.WeatherCondition
+import com.glpalma.simpleweather.domain.model.WeatherReport
+
+@Composable
+fun WeatherRoute(
+    viewModel: WeatherViewModel = hiltViewModel()
+) {
+    val state by viewModel.state.collectAsState()
+    LaunchedEffect(key1=Unit) {
+        viewModel.loadWeather(
+            lat = -23.55, // São Paulo, hardcoded for now
+            lon = -46.63
+        )
+    }
+
+    WeatherScreen(state)
+}
+
+@Composable
+fun WeatherScreen(
+    state: WeatherUiState
+) {
+    when (state) {
+        WeatherUiState.Loading -> {
+            Text(
+                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                text = "Loading...",
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace,
+                textAlign = TextAlign.Center,
+
+            )
+        }
+
+        is WeatherUiState.Success -> {
+            val report = state.report
+            Text(
+                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                text = "Temperature: ${report.current.temperatureC}",
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        is WeatherUiState.Error -> {
+            Text(
+                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                text = "Error: ${state.message}",
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeatherScreenLoadingPreview() {
+    WeatherScreen(
+        state = WeatherUiState.Loading
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeatherScreenSuccessPreview() {
+    WeatherScreen(
+        state = WeatherUiState.Success(report = fakeWeatherReport())
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeatherScreenErrorPreview() {
+    WeatherScreen(
+        state = WeatherUiState.Error("Network error")
+    )
+}
+
+fun fakeWeatherReport() = WeatherReport(
+    current = CurrentWeather(
+        temperatureC = 23.0,
+        condition = WeatherCondition.CLEAR),
+        daily = emptyList()
+)

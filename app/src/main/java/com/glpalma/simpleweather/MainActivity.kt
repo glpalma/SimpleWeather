e780c085a8c6a8ec89aa2d8b2dd0125213f0dc66
Gustavo Palma <gustavolpalma@gmail.com>
2026-02-23 15:27:15 -0300

    Set system and nav bars to be transparent

--- app/src/main/java/com/glpalma/simpleweather/MainActivity.kt
+++ app/src/main/java/com/glpalma/simpleweather/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.glpalma.simpleweather
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.glpalma.simpleweather.ui.theme.SimpleWeatherTheme
@@ -12,7 +14,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        val lightTransparentStyle = SystemBarStyle.dark(
+            scrim = TRANSPARENT,
+        )
+        enableEdgeToEdge(
+            statusBarStyle = lightTransparentStyle,
+            navigationBarStyle = lightTransparentStyle
+        )
         setContent {
             SimpleWeatherTheme(
                 darkTheme = true,

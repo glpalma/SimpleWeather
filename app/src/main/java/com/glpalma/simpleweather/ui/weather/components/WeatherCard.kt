6f876ea6c8498dc0175977f90fcebf6f39abd021
Gustavo Palma <gustavolpalma@gmail.com>
2026-02-24 17:48:05 -0300

    Added a border around the WeatherCard.kt to make it prettier

--- app/src/main/java/com/glpalma/simpleweather/ui/weather/components/WeatherCard.kt
+++ app/src/main/java/com/glpalma/simpleweather/ui/weather/components/WeatherCard.kt
@@ -5,16 +5,17 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.glpalma.simpleweather.ui.theme.WeatherCardBottom
@@ -25,15 +26,13 @@ private const val ANIM_DURATION = 400
 
 @Composable
 fun WeatherCard(
-    mode: WeatherScreenMode,
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    mode: WeatherScreenMode, modifier: Modifier = Modifier, content: @Composable () -> Unit
 ) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
 
     val targetHeightFraction = when (mode) {
         WeatherScreenMode.TODAY -> 0.75f
-        WeatherScreenMode.SEVEN_DAY -> 0.28f
+        WeatherScreenMode.SEVEN_DAY -> 0.35f
         WeatherScreenMode.CITY_PICKER -> 1f
     }
 
@@ -54,6 +53,11 @@ fun WeatherCard(
             .fillMaxWidth()
             .height(screenHeight * heightFraction)
             .clip(RoundedCornerShape(bottomStart = cornerRadius, bottomEnd = cornerRadius))
+            .border(
+                1.dp,
+                Color.White.copy(alpha = 0.3f),
+                shape = RoundedCornerShape(bottomStart = cornerRadius, bottomEnd = cornerRadius)
+            )
             .background(
                 Brush.verticalGradient(
                     colors = listOf(WeatherCardTop, WeatherCardBottom)
